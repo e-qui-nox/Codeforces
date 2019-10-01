29b101fbcb6ebbc8fccd7bef240286e84dd72e53
Athi Dsh <32543864+Athi101@users.noreply.github.com>
2019-10-01 22:05:28 +0530

    Update 268A.java

--- problemset/268A.java
+++ problemset/268A.java
@@ -3,27 +3,27 @@ import java.lang.*;
 
 public class 268A {
 	public static void main (String[] args) {
-		Scanner scan = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
-		int n = scan.nextInt();
+		int n = sc.nextInt();
 		
 		int[] home = new int[n];
 		int[] away = new int[n];
 		
 		for(int i=0; i<n; i++) {
-		    home[i] = scan.nextInt();
-		    away[i] = scan.nextInt();
+		    home[i] = sc.nextInt();
+		    away[i] = sc.nextInt();
 		}
 		
 		int count = 0;
 		for(int i=0; i<n; i++)
 		    for(int j=0; j<n; j++) {
-		        if(home[i] == away[j])
+		        if(home[i] == away[j]) //comparing each value in the home array with every values in the away array
 		            count++;
 		    }
 		
 		System.out.println(count);
 		
-		scan.close();
+		sc.close();
 	}
 }
